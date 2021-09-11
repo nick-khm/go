6750ad2938e883de29ffe303a3729d7d3f82442a
Nick Kharaim <nick.kharaim@gmail.com>
2021-09-11 09:39:27 +0200

    Struct

--- src/training.go/helloworld/main.go
+++ src/training.go/helloworld/main.go
@@ -1,76 +1,17 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
-)
-
-func ProcessLine(line, old, new string) (found bool, res string, occ int) {
-	oldLower := strings.ToLower(old)
-	newLower := strings.ToLower(new)
-	res = line
-	if strings.Contains(res, old) || strings.Contains(res, oldLower) {
-		found = true
-		occ += strings.Count(res, old)
-		occ += strings.Count(res, oldLower)
-		res = strings.Replace(res, old, new, -1)
-		res = strings.Replace(res, oldLower, newLower, -1)
-	}
-	return found, res, occ
+type Address struct {
+	street, city string
 }
 
-func FindAndReplace(srcFile, old, new, dst string) (occ int, lines []int, err error) {
-	file, err := os.Open(srcFile)
-	if err != nil {
-		return occ, lines, err
-	}
-	defer file.Close()
-
-	dstFile, err := os.Create(dst)
-	if err != nil {
-		return occ, lines, err
-	}
-	defer dstFile.Close()
-	scanner := bufio.NewScanner(file)
-	writer := bufio.NewWriter(dstFile)
-	defer writer.Flush()
-
-	old += " "
-	new += " "
-	lineIdx := 1
-	for scanner.Scan() {
-		found, res, o := ProcessLine(scanner.Text(), old, new)
-		if found {
-			occ += o
-			lines = append(lines, lineIdx)
-		}
-		fmt.Fprintf(writer, res)
-		lineIdx++
-	}
-	return occ, lines, nil
+type Person struct {
+	Name string
+	Age  int
+	Addr Address
 }
 
 func main() {
-	old := "Go"
-	new := "Python"
-	occ, lines, err := FindAndReplace("wikigo.txt", old, new, "output.txt")
-	if err != nil {
-		fmt.Printf("Error while executing find replace: %v\n", err)
-	}
-	fmt.Println("=== Summary ===")
-	defer fmt.Println("=== END Summary ===")
-	fmt.Printf("Number of occurences of %v: %v\n", old, occ)
-	fmt.Printf("Number of lines: %d\n", len(lines))
-
-	len := len(lines)
-	fmt.Print("Lines [ ")
-	for i, l := range lines {
-		fmt.Print(l)
-		if i < len-1 {
-			fmt.Print(" - ")
-		}
-	}
-	fmt.Println(" ]")
+	var p Person
+	p.Name = "Bob"
+	p.Addr.city = "Lion"
 }

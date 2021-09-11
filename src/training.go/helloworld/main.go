ae748968147dfbb7fe70e906660d139d0c97d579
Nick Kharaim <nick.kharaim@actility.com>
2021-09-11 10:14:28 +0200

    Struct declarations

--- src/training.go/helloworld/main.go
+++ src/training.go/helloworld/main.go
@@ -1,17 +1,34 @@
 package main
 
-type Address struct {
-	street, city string
-}
+import (
+	"fmt"
 
-type Person struct {
-	Name string
-	Age  int
-	Addr Address
-}
+	"helloworld.com/greetings/src/training.go/helloworld/input/player"
+)
 
 func main() {
-	var p Person
-	p.Name = "Bob"
-	p.Addr.city = "Lion"
+	var p1 player.Player
+	p1.Name = "Bob"
+	p1.Age = 10
+
+	fmt.Printf("Player 1: %v\n", p1)
+	fmt.Printf("p1.Name=%v, p1.Age=%v\n", p1.Name, p1.Age)
+
+	a := player.Avatar{"http://lol.com/ag.jpg"}
+	fmt.Printf("avatar: %v\n", a)
+
+	p2 := player.Player{
+		Name: "John",
+		Age:  25,
+		Avatar: player.Avatar{
+			Url: "http://domain/a.jpg",
+		},
+	}
+	fmt.Printf("Player 2: %v\n", p2)
+
+	p3 := player.New("Bobette")
+	p3.Avatar = a
+	fmt.Printf("Player 3: %v\n", p3)
+	a.Url = "totourl"
+	fmt.Printf("Player 3: %v\n", p3)
 }

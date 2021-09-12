35d339a55b5ec060755a0b625ce865bc9fcb0e75
Nick Kharaim <nick.kharaim@actility.com>
2021-09-12 14:40:56 +0200

    Embedded struct

--- src/training.go/helloworld/main.go
+++ src/training.go/helloworld/main.go
@@ -2,33 +2,25 @@ package main
 
 import (
 	"fmt"
-
-	"helloworld.com/greetings/src/training.go/helloworld/input/player"
 )
 
+type User struct {
+	Name  string
+	Email string
+}
+
+type Admin struct {
+	User
+	Level int
+}
+
 func main() {
-	var p1 player.Player
-	p1.Name = "Bob"
-	p1.Age = 10
-
-	fmt.Printf("Player 1: %v\n", p1)
-	fmt.Printf("p1.Name=%v, p1.Age=%v\n", p1.Name, p1.Age)
-
-	a := player.Avatar{"http://lol.com/ag.jpg"}
-	fmt.Printf("avatar: %v\n", a)
-
-	p2 := player.Player{
-		Name: "John",
-		Age:  25,
-		Avatar: player.Avatar{
-			Url: "http://domain/a.jpg",
-		},
+	u := User{
+		Name:  "Bob",
+		Email: "bob@golang.com",
 	}
-	fmt.Printf("Player 2: %v\n", p2)
+	fmt.Printf("User=%v\n", u)
 
-	p3 := player.New("Bobette")
-	p3.Avatar = a
-	fmt.Printf("Player 3: %v\n", p3)
-	a.Url = "totourl"
-	fmt.Printf("Player 3: %v\n", p3)
+	a := Admin{Level: 2, User: User{Name: "Alice", Email: "alice@golang.com"}}
+	fmt.Printf("Admin=%v\n", a)
 }

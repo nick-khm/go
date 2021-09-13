ffbedca1ef33d13d9540e45c6257fc762c787f59
Nick Kharaim <nick.kharaim@gmail.com>
2021-09-13 12:58:15 +0200

    Pointers

--- src/training.go/helloworld/main.go
+++ src/training.go/helloworld/main.go
@@ -4,20 +4,75 @@ import (
 	"fmt"
 )
 
-type Rect struct {
-	Width, Height int
+func UpdateVal(val string) {
+	val = "myValue"
 }
 
-func (r Rect) Area() int {
-	return r.Width * r.Height
-}
-
-func (r Rect) String() string {
-	return fmt.Sprintf("Rect ==> width=%v, height=%v", r.Width, r.Height)
+func UpdatePointer(ptr *string) {
+	*ptr = "myValue"
 }
 
 func main() {
-	r := Rect{22, 89}
-	fmt.Printf("Area: %v\n", r.Area())
-	fmt.Println(r)
+	i := 1
+	var p *int = &i
+
+	fmt.Printf("i=%v\n", i)
+	fmt.Printf("p=%v\n", p)
+	fmt.Printf("*p=%v\n", *p)
+	fmt.Printf("p=%v\n", p)
+
+	fmt.Println("-----------------")
+
+	s := "Bob"
+	sPtr := &s
+	s2 := *sPtr
+
+	fmt.Printf("s=%v\n", s)
+	fmt.Printf("sPrt=%v\n", sPtr)
+	fmt.Printf("*sPrt=%v\n", *sPtr)
+	fmt.Printf("s2=%v\n", s2)
+
+	fmt.Println("-----------------")
+
+	s = "Alice"
+
+	fmt.Println("Change the initial variable from Bob to Alice")
+	fmt.Printf("s=%v\n", s)
+	fmt.Printf("sPrt=%v\n", sPtr)
+	fmt.Printf("*sPrt=%v\n", *sPtr)
+	fmt.Printf("s2=%v\n", s2)
+
+	fmt.Println("-----------------")
+
+	*sPtr = "Hugo"
+
+	fmt.Println("Dereference sPrt and update it with Hugo")
+	fmt.Printf("s=%v\n", s)
+	fmt.Printf("sPrt=%v\n", sPtr)
+	fmt.Printf("*sPrt=%v\n", *sPtr)
+	fmt.Printf("s2=%v\n", s2)
+
+	fmt.Println("-----------------")
+
+	UpdateVal(s)
+
+	fmt.Println("UpdateVal => trying to change to myValue")
+	fmt.Printf("s=%v\n", s)
+	fmt.Printf("sPrt=%v\n", sPtr)
+	fmt.Printf("*sPrt=%v\n", *sPtr)
+	fmt.Printf("s2=%v\n", s2)
+
+	fmt.Println("-----------------")
+
+	// UpdatePointer(&s)
+	UpdatePointer(sPtr)
+
+	fmt.Println("UpdatePointer => trying to change to myValue")
+	fmt.Printf("s=%v\n", s)
+	fmt.Printf("sPrt=%v\n", sPtr)
+	fmt.Printf("*sPrt=%v\n", *sPtr)
+	fmt.Printf("s2=%v\n", s2)
+
+	fmt.Println("-----------------")
+
 }

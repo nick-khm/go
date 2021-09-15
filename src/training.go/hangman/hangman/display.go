3bcde54e117c492c004fd35616c60cad1fe3803f
Nick Kharaim <nick.kharaim@gmail.com>
2021-09-15 22:29:16 +0200

    Game state

---
+++ src/training.go/hangman/hangman/display.go
@@ -0,0 +1,163 @@
+package hangman
+
+import "fmt"
+
+func DrawWelcome() {
+	fmt.Println(`
+	 /$$   /$$                                                                
+	| $$  | $$                                                                
+	| $$  | $$  /$$$$$$  /$$$$$$$   /$$$$$$  /$$$$$$/$$$$   /$$$$$$  /$$$$$$$ 
+	| $$$$$$$$ |____  $$| $$__  $$ /$$__  $$| $$_  $$_  $$ |____  $$| $$__  $$
+	| $$__  $$  /$$$$$$$| $$  \ $$| $$  \ $$| $$ \ $$ \ $$  /$$$$$$$| $$  \ $$
+	| $$  | $$ /$$__  $$| $$  | $$| $$  | $$| $$ | $$ | $$ /$$__  $$| $$  | $$
+	| $$  | $$|  $$$$$$$| $$  | $$|  $$$$$$$| $$ | $$ | $$|  $$$$$$$| $$  | $$
+	|__/  |__/ \_______/|__/  |__/ \____  $$|__/ |__/ |__/ \_______/|__/  |__/
+								   /$$  \ $$                                  
+								  |  $$$$$$/                                  
+								   \______/                                   
+	`)
+}
+
+func Draw(g *Game, guess string) {
+	drawTurns(g.TurnsLeft)
+	drawState(g, guess)
+}
+
+func drawState(g *Game, guess string) {
+	fmt.Print("Guessed: ")
+	drawLetters(g.FoundLetters)
+
+	fmt.Print("Used: ")
+	drawLetters(g.UsedLetters)
+
+	switch g.State {
+	case "goodGuess":
+		fmt.Print("Good guess!")
+	case "alreadyGuessed":
+		fmt.Printf("Letter %v was already user", guess)
+	case "badGuess":
+		fmt.Printf("Bad guess, '%v' is not in the word", guess)
+	case "lost":
+		fmt.Print("You lost :( The word was: ")
+		drawLetters(g.Letters)
+	case "win":
+		fmt.Print("You won! The word was: ")
+		drawLetters(g.Letters)
+	}
+}
+
+func drawLetters(l []string) {
+	for _, c := range l {
+		fmt.Printf("%v", c)
+	}
+	fmt.Println()
+}
+
+func drawTurns(l int) {
+	var draw string
+	switch l {
+	case 0:
+		draw = `
+		____
+	   |    |      
+	   |    o      
+	   |   /|\     
+	   |    |
+	   |   / \
+	  _|_
+	 |   |______
+	 |          |
+	 |__________|
+			`
+	case 1:
+		draw = `
+		____
+	   |    |      
+	   |    o      
+	   |   /|\     
+	   |    |
+	   |    
+	  _|_
+	 |   |______
+	 |          |
+	 |__________|
+			`
+	case 2:
+		draw = `
+		____
+	   |    |      
+	   |    o      
+	   |    |
+	   |    |
+	   |     
+	  _|_
+	 |   |______
+	 |          |
+	 |__________|
+			`
+	case 3:
+		draw = `
+		____
+	   |    |      
+	   |    o      
+	   |        
+	   |   
+	   |   
+	  _|_
+	 |   |______
+	 |          |
+	 |__________|
+			`
+	case 4:
+		draw = `
+		____
+	   |    |      
+	   |      
+	   |      
+	   |  
+	   |  
+	  _|_
+	 |   |______
+	 |          |
+	 |__________|
+			`
+	case 5:
+		draw = `
+		____
+	   |        
+	   |        
+	   |        
+	   |   
+	   |   
+	  _|_
+	 |   |______
+	 |          |
+	 |__________|
+			`
+	case 6:
+		draw = `
+		
+	   |     
+	   |     
+	   |     
+	   |
+	   |
+	  _|_
+	 |   |______
+	 |          |
+	 |__________|
+			`
+	case 7:
+		draw = `
+	  _ _
+	 |   |______
+	 |          |
+	 |__________|
+			`
+	case 8:
+		draw = `
+	
+			`
+	}
+	fmt.Println(draw)
+}
--- src/training.go/hangman/main.go
+++ src/training.go/hangman/main.go
@@ -9,12 +9,21 @@ import (
 
 func main() {
 	g := hangman.New(8, "Golang")
-	fmt.Println(g)
 
-	l, err := hangman.ReadGuess()
-	if err != nil {
-		fmt.Printf("Cound not read from terminal: %v", err)
-		os.Exit(1)
+	hangman.DrawWelcome()
+	guess := ""
+	for {
+		hangman.Draw(g, guess)
+		switch g.State {
+		case "win", "lost":
+			os.Exit(0)
+		}
+		l, err := hangman.ReadGuess()
+		if err != nil {
+			fmt.Printf("Cound not read from terminal: %v", err)
+			os.Exit(1)
+		}
+		guess = l
 	}
-	fmt.Println(l)
+
 }

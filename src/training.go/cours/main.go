1486e2f5004fea7ef17e67982794f71999a74e01
Nick Kharaim <nick.kharaim@gmail.com>
2021-09-23 23:45:33 +0200

    Loop on map

--- src/training.go/cours/main.go
+++ src/training.go/cours/main.go
@@ -7,34 +7,23 @@ type Vector struct {
 }
 
 func main() {
-	m := make(map[string]int)
-	fmt.Printf("Map content %v len(%v)\n", m, len(m))
-
-	m["hello"] = 5
-	m["good bye"] = 10
-	fmt.Printf("Map content %v len(%v)\n", m, len(m))
-	fmt.Printf("key=hello key=%v\n", m["hello"])
-
-	// presence of a key
-	j, ok := m["toto"]
-	fmt.Printf("j=%v ok=%v\n", j, ok)
-
-	// presence of a key
-	d, okk := m["hello"]
-	fmt.Printf("d=%v okk=%v\n", d, okk)
-
-	m["beatles"] = 2
-	if _, ok := m["beatles"]; ok {
-		fmt.Println("beatles key exists")
+	m := map[string]int{
+		"Bob":     10,
+		"Alice":   15,
+		"Bobette": 20,
+		"John":    7,
 	}
 
-	delete(m, "beatles")
-	fmt.Printf("Map content %v len(%v)\n", m, len(m))
+	for name, age := range m {
+		fmt.Printf("name=%v, age=%v\n", name, age)
+	}
 
-	m2 := m
-	fmt.Printf("m2 content %v (len=%v)\n", m2, len(m2))
+	i := 1
+	for name := range m {
+		fmt.Printf("name=%v\n", name)
+		m[name] = i
+		i++
+	}
+	fmt.Println(m)
 
-	m2["help"] = 44
-	fmt.Printf("m content %v (len=%v)\n", m, len(m))
-	fmt.Printf("m2 content %v (len=%v)\n", m2, len(m2))
 }

0489528780b40b08543b8ee43ecf1df640417ef6
Nick Kharaim <nick.kharaim@gmail.com>
2021-10-04 21:46:01 +0200

    Init Dictionary struct and init Badger DB

--- src/training.go/dictionary/main.go
+++ src/training.go/dictionary/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
 
+	"helloworld.com/greetings/src/training.go/dictionary/dictionary"
+)
+
+func main() {
+	d, err := dictionary.New("./badger")
+	handleErr(err)
+	defer d.Close()
+}
+
+func handleErr(err error) {
+	if err != nil {
+		fmt.Printf("Dictionary error: %v\n", err)
+		os.Exit(1)
+	}
 }

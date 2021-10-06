74ddd20d3522f6121606ae4df3f68a7c34dd0125
Nick Kharaim <nick.kharaim@gmail.com>
2021-10-06 21:18:06 +0200

    Get function

--- src/training.go/dictionary/main.go
+++ src/training.go/dictionary/main.go
@@ -11,6 +11,10 @@ func main() {
 	d, err := dictionary.New("./badger")
 	handleErr(err)
 	defer d.Close()
+
+	d.Add("golang", "wonderful language")
+	entry, _ := d.Get("golang")
+	fmt.Println(entry)
 }
 
 func handleErr(err error) {

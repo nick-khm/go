c0d265d80f084f369104d3031c13e93d056cbeec
Nick Kharaim <nick.kharaim@gmail.com>
2021-10-28 21:12:07 +0200

    Interface and types

--- src/training.go/empty-interface/main.go
+++ src/training.go/empty-interface/main.go
@@ -20,7 +20,43 @@ func (c Chef) Cook() {
 	fmt.Printf("I'm cooking with %v stars\n", c.Stars)
 }
 
+func processPerson(i interface{}) {
+	switch p := i.(type) {
+	case Person:
+		fmt.Printf("We have a person=%v\n", p)
+	case Chef:
+		fmt.Printf("We have a chef=%v. Let him cook!", p)
+		p.Cook()
+	default:
+		fmt.Printf("Unknown type=%T, value=%v\n", p, p)
+	}
+}
+
 func main() {
 	var x interface{} = Person{"Bob", 10}
 	fmt.Printf("x type=%T, data=%v\n", x, x)
+
+	s, ok := x.(string)
+	fmt.Printf("Person as string ok? %v. value=%v\n", ok, s)
+	n, ok := x.(int)
+	fmt.Printf("Person as int ok? %v. value=%v\n", ok, n)
+
+	processPerson(x)
+
+	x = Chef{
+		Stars: 4,
+		Person: Person{
+			Name: "Alice",
+			Age:  22,
+		},
+	}
+
+	processPerson(x)
+
+	x = 3
+	processPerson(x)
+
+	x = "John"
+	processPerson(x)
+
 }

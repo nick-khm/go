8ec286a105b0bc3a3d58c11258edbf8b0b89bd05
Nick Kharaim <nick.kharaim@gmail.com>
2021-11-02 13:54:18 +0100

    Http reader

---
+++ src/training.go/reader/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	resp, err := http.Get("https://golang.org")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(body))
+}
